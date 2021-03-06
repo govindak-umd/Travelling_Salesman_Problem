69b9036bdbfc058f933b17680fd50bb8cdc1403e
Govind <govindajithkumar@gmail.com>
2021-03-06 18:49:48 -0500

    README Update

---
+++ cmake-build-debug/CMakeFiles/Travelling_Salesman_Problem.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Travelling_Salesman_Problem.dir/src/TravellingSalesman.cpp.obj"
+  "CMakeFiles/Travelling_Salesman_Problem.dir/src/main.cpp.obj"
+  "Travelling_Salesman_Problem.exe"
+  "Travelling_Salesman_Problem.exe.manifest"
+  "Travelling_Salesman_Problem.pdb"
+  "libTravelling_Salesman_Problem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Travelling_Salesman_Problem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
